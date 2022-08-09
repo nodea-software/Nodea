d063664ce750a2a5ccf646d3258bccef17590b24
stephane.bourdier@newmips.com <steph63b@hotmail.fr>
2022-08-09 15:54:08 +0200

    - Remove code platform conf

--- config/code_platform.js
+++ config/code_platform.js
@@ -2,11 +2,11 @@ const globalConf = require('./global');
 
 const config = {
 	develop: {
-		enabled: true, // Connect to code plateform
+		enabled: false, // Connect to code plateform
 		platform: process.env.CODE_PLATFORM || "gitlab", // Gitlab || TODO Github || TODO Bitbucket
-		protocol: process.env.GITLAB_PROTOCOL || "https",
-		url: process.env.GITLAB_URL || "gitlab.nodea.studio",
-		token: process.env.GITLAB_PRIVATE_TOKEN || "bfCdSxRKJ4F7rsCzbcsU"
+		protocol: process.env.GITLAB_PROTOCOL || "http",
+		url: process.env.GITLAB_URL || "",
+		token: process.env.GITLAB_PRIVATE_TOKEN || ""
 	},
 	studio: {
 		enabled: true,
