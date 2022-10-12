a8908c7ca34b757df3ee08af36f7140d3af3a51b
FLafaye <florian.lafaye@newmips.com>
2022-10-12 15:20:23 +0200

    - Fix error on add component status with name too long

--- structure/template/config/database.js
+++ structure/template/config/database.js
@@ -3,7 +3,7 @@ const globalConf = require('./global');
 const databaseConf = {
 	develop: {
 		host: process.env.APP_DB_IP || process.env.DATABASE_IP || '127.0.0.1',
-		port: process.env.APP_DB_PORT || '3306', // mysql: 3306 - postgres: 5432
+		port: process.env.APP_DB_PORT || '3307', // mysql: 3306 - postgres: 5432
 		user: process.env.APP_DB_USER || 'node@',
 		password: process.env.APP_DB_PWD || 'node@_pwd',
 		database: process.env.APP_DB_NAME || 'node@',
