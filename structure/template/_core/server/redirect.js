980f79253d41467d5032ba919cd34182728d6abf
Jean-Félix BRILLANT <jean-felix.brillant@newmips.com>
2024-02-19 17:54:15 +0100

    fix Redirect to send ajaxData
    Signed-off-by: Jean-Félix BRILLANT <jean-felix.brillant@newmips.com>

--- structure/template/_core/server/redirect.js
+++ structure/template/_core/server/redirect.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
 	const redirect = res.redirect;
-	res.redirect = view => {
+	res.redirect = (view, ajaxData = {}) => {
 		// If request comes from ajax call, no need to render show/list/etc.. pages, 200 status is enough
 		if (req.query.ajax) {
 			// Check role access error in toastr. Send 403 if found, {refresh: true} will force reload of the page
@@ -10,7 +10,7 @@ module.exports = (req, res, next) => {
 				if (toast.message && toast.message == "administration.access_settings.no_access_role")
 					return res.status(403).send({refresh: true});
 			}
-			return res.sendStatus(200);
+			return res.status(200).send(ajaxData);
 		}
 		redirect.call(res, view);
 	}
